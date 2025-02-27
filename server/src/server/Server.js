2da8cc7d132fa416e1db2e8b1acf3fe6e48c682e
neomoth <69372252+neomoth@users.noreply.github.com>
2025-02-27 15:31:57 -0500

    add logMeOut api route

--- server/src/server/Server.js
+++ server/src/server/Server.js
@@ -123,9 +123,9 @@ export class Server {
 				// console.log(ws.token)
 				try {
 					this.stats.totalConnections++;
+					await client.setStatus("Logging in...", true, true);
 					let client = this.clients.createClient(ws);
 					ws.client = client;
-					await client.setStatus("Logging in...", true, true);
 					if(!await client.checkIsLoggedIn()) client.destroy();
 					await client.setStatus("Logged in!", true);
 					client.startProtocol();
